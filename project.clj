b2805e4775824dd1f67ad2c8b551700877cfaabf
Abel Quiros <beluchin@gmail.com>
2018-12-02 23:48:15 +0000

    squiggly clojure config

--- project.clj
+++ project.clj
@@ -3,7 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  ;; flycheck-clojure setup
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.typed "0.5.3"  :classifier "slim"]]
+  ;; Use lein-environ plugin if you want to set checker configuration in profile.
+  :plugins [[lein-environ "1.1.0"]]
+  ;; Configuration here may be overridden by namespace metadata.
+  :profiles {:dev {:env {:squiggly {:checkers [:eastwood :typed :kibit]
+                                    :eastwood-exclude-linters [:unlimited-use]
+                                    :eastwood-options {:add-linters [:unused-fn-args]
+                                                       ;; :builtin-config-files ["myconfigfile.clj"]
+                                                       }}}}}
   :main mastermind.core
 
   ; to evaluate inside defproject ...
