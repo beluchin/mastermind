63263d1a8ec8b8d5f1f15d2f46deb5000ba395b1
Abel Quiros <beluchin@gmail.com>
2018-12-09 08:56:22 +0000

    app.level replaces mastermind

--- src/mastermind.clj
+++
@@ -1,6 +0,0 @@
-(ns mastermind)
-
-(def levels
-  {:easy {:num-digits 3, :dups false, :digits (set (range 0 6))}
-   :hard {:num-digits 4, :dups false, :digits (set (range 0 10))}
-   :expert {:num-digits 4, :dups true, :digits (set (range 0 10))}})
