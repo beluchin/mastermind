21737f41d133ad6556c8d48222fe63347f5760fd
Abel Quiros <beluchin@gmail.com>
2018-12-19 09:00:11 +0000

    digits are shown sorted when level is printed

--- src/mastermind/app/console.clj
+++ src/mastermind/app/console.clj
@@ -1,6 +1,10 @@
 (ns mastermind.app.console)
 
-(defn print-level [_] (throw (UnsupportedOperationException.)))
+(defn printable [level]
+  (update level :digits #(sort (vec %))))
+
+(defn print-level [l]
+  (print (printable l)))
 
 (defn game-started [_]
   (print-level 42))
--- test/mastermind/app/console_test.clj
+++ test/mastermind/app/console_test.clj
@@ -7,3 +7,7 @@
   (with-redefs [console/print-level (spy/mock (fn [_] nil))] 
     (console/game-started "I-guess")
     (is (spy/called-once? console/print-level))))
+
+(deftest digits-are-sorted-on-printable-level
+  (let [level {:digits #{1 2 3}}]
+    (is (= (:digits (console/printable level)) [1 2 3]))))
