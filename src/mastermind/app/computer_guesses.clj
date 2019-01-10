1854599318425da92704963f584d15d4bfb71ba6
Abel Quiros <beluchin@gmail.com>
2019-01-10 09:33:39 +0000

    computer-guesses happy path

--- src/mastermind/app/computer_guesses.clj
+++ src/mastermind/app/computer_guesses.clj
@@ -1,12 +1,26 @@
 (ns mastermind.app.computer-guesses
-  (:require [mastermind.domain :as domain]
-            [mastermind.app.level :as level]))
+  (:require [clojure.string :as s]
+            [mastermind.app.console :as console]
+            [mastermind.app.level :as level]
+            [mastermind.app.utils :as utils]
+            [mastermind.domain :as domain]
+            [mastermind.domain.combination :as combination]))
 
-(defrecord Game [level]
+(def ^:const default-level (:expert level/levels))
+
+(defn answer-or-error [txt]
+  (let [[ok so-so] (map utils/int-or-nil (s/split txt #" "))]
+    {:ok ok :so-so so-so}))
+
+(defrecord Game [level possible-solutions]
   domain/Playable
-  (get-next-guess [_])
-  (get-answer [_ guess])
+  (get-next-guess [_] (rand-nth possible-solutions))
+  (get-answer [_ guess]
+    (console/display guess)
+    (console/read-until-no-error answer-or-error))
   (notify [_ guess answer])
-  (num-digits [_]))
+  (num-digits [_] (:num-digits level)))
 
-(defn new-game [] (->Game (:expert level/levels)))
+(defn new-game 
+  [] 
+  (->Game default-level (combination/all-combinations default-level)))
--- test/mastermind/core_test.clj
+++ test/mastermind/core_test.clj
@@ -5,7 +5,8 @@
             [mastermind.domain :as domain]
             [mastermind.app.console :as console]
             [mastermind.app.user-guesses :as user-guesses]
-            [mastermind.domain.combination :as combination]))
+            [mastermind.domain.combination :as combination]
+            [mastermind.app.computer-guesses :as computer-guesses]))
 
 (t/deftest user-guesses-end-to-end
   (let [hidden (combination/get-combination user-guesses/default-level)]
@@ -14,7 +15,10 @@
       (t/is (nil? (sut/-main "I-guess"))))))
 
 (t/deftest computer-guesses-end-to-end 
-  (t/is (nil? (sut/-main "computer-guesses"))))
+  (let [level computer-guesses/default-level]
+    (with-redefs
+      [read-line (constantly (str (:num-digits level) " 0"))]
+      (t/is (nil? (sut/-main "computer-guesses"))))))
 
 (t/deftest prints-level-when-game-starts
   (with-redefs [sut/print-level (spy/mock (fn [_] nil))
